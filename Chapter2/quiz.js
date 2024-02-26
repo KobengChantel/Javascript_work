d309b60b290a810a1db0b1894353e275c17d8eaa
KobengChantel <kobengchantel@gmail.com>
2024-02-26 13:48:14 +0200

    i am done with 'original/main'.

---
+++ Chapter2/quiz.js
@@ -0,0 +1,38 @@
+// 1. string
+const c = "5";
+
+// 2. number
+const c = 91;
+
+// 3. Which one is generally better, line 1 or line 2
+let empty1 = undefined; //line 1
+let empty2 = null; //line 2
+//answer 
+line 2
+
+// 4. output = world
+let a = "Hello";
+a = "world";
+console.log(a);
+
+// 5. What will be logged to the console?
+let a = "world";
+let b = `Hello ${a}!`;
+console.log(b);
+//answer
+Hello world
+
+//6. What is the value of a? 
+let a = "Hello";
+a = prompt("world");
+console.log(a);2
+//answer
+Hello
+
+// 7. What is the value of b output to the console? 
+let a = 5;
+let b = 70;
+let c = "5";
+b++;
+console.log(b)
+//answer
