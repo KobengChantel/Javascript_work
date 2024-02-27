c618eeb620e6666b8228a3f07ac2f3f144d05b0a
KobengChantel <kobengchantel@gmail.com>
2024-02-28 01:06:23 +0200

    save all changes made to files

--- Chapter3/EXERCISE3.1.js
+++ Chapter3/EXERCISE3.1.js
@@ -1,4 +1,7 @@
+//array
 shoppingList = ["Milk", "Bread","Apples"]
+// length i
 console.log(shoppingList.length);
+//Update "Bread" to "Bananas."
 shoppingList[1] = "Bananas";
 console.log(shoppingList);
